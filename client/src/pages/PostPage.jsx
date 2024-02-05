d75d7f92be1774e1a86223c40a4d32e4fcae21c1
Vishal_Gaurav <vishalgaurav293@gmail.com>
2024-02-05 06:13:18 +0530

    Call to acction

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-// import CallToAction from '../components/CallToAction';
+import CallToAction from '../components/CalltoAction';
 // import CommentSection from '../components/CommentSection';
 // import PostCard from '../components/PostCard';
 
@@ -70,7 +70,7 @@ export default function PostPage() {
           {post && post.category}
         </Button>
       </Link>
-      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+      {/* <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
       <img
         src={post && post.image}
         alt={post && post.title}
@@ -86,13 +86,43 @@ export default function PostPage() {
       <h3 className='text-1xl mt-6 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
        Pincode : {post && post.pinCode}
       </h3>
-        <div
+        {/* <div
+        className='p-3 max-w-2xl mx-auto w-full post-content'
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div> *
+        </div>
+      </div>  */}
+      <div className='flex flex-col mt-4 sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+            City: {post && post.city}
+            </h2>
+            <h2 className='text-2xl'>
+            Price: 300 
+            </h2>
+            <h2 className='text-2xl'>
+            Pincode: {post && post.pinCode}
+            </h2>
+            <h2 className='text-2xl'>
+            Phone No : {post && post.aadhar}
+            </h2>
+            <p className='text-gray-500 my-2'>
+            <div
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
+                    Send Request
+                </a>
+            </Button>
         </div>
-      </div>
-      
+        <div className="p-7 flex-1">
+            <img src={post && post.image}
+        alt={post && post.title} />
+        </div>
+    </div>
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
@@ -103,9 +133,9 @@ export default function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div> */}
-      {/* <div className='max-w-4xl mx-auto w-full'>
+      <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
-      </div> */}
+      </div>
       {/* <CommentSection postId={post._id} /> */}
 
       {/* <div className='flex flex-col justify-center items-center mb-5'>
